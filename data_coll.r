2c126387fe282f6fd1d5c5a2891f94263fc0f3a6
zzi2hoahn <zzi2ho@naver.com>
2022-09-21 21:26:07 +0900

    2022-09-21 --commit

--- data_coll.r
+++ data_coll.r
@@ -20,4 +20,51 @@ datelist[1:3]          # 확인
 
 #---# [4단계: 인증키 입력하기]
 
-service_key <- ""  # 인증키 입력
+service_key <- "ZmaYLlwZwinwSmJgxh2mQCRL3LxIeHpjgcPe%2Bw5g9qFTXNdOnUzsMAXdgieySt6gX4lGlNQmYiwEXQkSARFT5w%3D%3D"  # 인증키 입력
+
+
+#03-02 요청목록 생성 : 자료를 어떻게 요청할까?
+
+#---# [1단계 : 요청 목록 만들기]
+
+url_list <- list()
+cnt <- 0
+
+#---# [2단계 : 요청 목록 채우기]
+
+for(i in 1:nrow(loc)){           # 외부반복: 25개 자치구
+  for(j in 1:length(datelist)){  # 내부반복: 12개월
+    cnt <- cnt + 1               # 반복누적 카운팅
+    #---# 요청 목록 채우기 (25 X 12= 300)
+    url_list[cnt] <- paste0("http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?",
+                            "LAWD_CD=", loc[i,1],         # 지역코드
+                            "&DEAL_YMD=", datelist[j],    # 수집월
+                            "&numOfRows=", 100,           # 한번에 가져올 최대 자료 수
+                            "&serviceKey=", service_key)  # 인증키
+  } 
+  Sys.sleep(0.1)   # 0.1초간 멈춤
+  msg <- paste0("[", i,"/",nrow(loc), "]  ", loc[i,3], " 의 크롤링 목록이 생성됨 => 총 [", cnt,"] 건") # 알림 메시지
+  cat(msg, "\n\n") 
+}
+
+#---# [3단계: 요청 목록 동작 확인]
+
+length(url_list)                # 요청목록 갯수 확인
+browseURL(paste0(url_list[1]))  # 정상작동 확인(웹브라우저 실행)
+
+
+#----------------
+# 3-3 크롤링 실행
+#----------------  
+
+#---# [1단계: 임시 저장 리스트 생성]
+
+library(XML)        
+library(data.table) 
+library(stringr)    
+
+raw_data <- list()        # xml 임시 저장소
+root_Node <- list()       # 거래내역 추출 임시 저장소
+total <- list()           # 거래내역 정리 임시 저장소
+dir.create("02_raw_data") # 새로운 폴더 만들기
+
