019aac38a611b56e07b6192229dcffd9d9b4c0fd
zzi2hoahn <zzi2ho@naver.com>
2022-10-14 01:43:22 +0900

    2022-10-12 --commit

--- 04_preprocessing.r
+++ 04_preprocessing.r
@@ -48,7 +48,7 @@ head(apt_price$apt_nm, 30)  # 아파트 이름 현황
 apt_price$apt_nm <- gsub("\\(.*","", apt_price$apt_nm) # 괄호이후 삭제
 head(apt_price$apt_nm, 30)                             # 아파트 이름 확인
 
-loc <- read.csv("./01_code/sigun_code/sigun_code.csv", fileEncoding="UTF-8")  # 지역코드 불러오기
+loc <- read.csv(""D:\r_project\sigun_code.csv"", fileEncoding="UTF-8")  # 지역코드 불러오기
 apt_price <- merge(apt_price, loc, by = 'code')         # 지역명 결합하기
 apt_price$juso_jibun <- paste0(apt_price$addr_2, " ", apt_price$dong," ",
                                apt_price$jibun," ",apt_price$apt_nm) # 주소조합
@@ -81,3 +81,55 @@ min(apt_price$floor)
 
 apt_price$cnt <- 1   # 모든 데이터에 숫자 1 할당
 head(apt_price, 2)   # 확인
+
+#---------------
+# 4-3 저장하기
+#---------------
+
+#---# [1단계: 필요칼럼 추출]
+
+apt_price <- apt_price %>% select(ymd, ym, year, code, addr_1, apt_nm, 
+              juso_jibun, price, con_year,  area, floor, py, cnt) # 칼럼 추출
+head(apt_price, 2)  # 확인
+
+#---# [2단계: 저장]
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+dir.create("./04_preprocess")   # 새로운 폴더 생성
+save(apt_price, file = "./04_preprocess/04_preprocess.rdata") # 저장
+write.csv(apt_price, "./04_preprocess/04_preprocess.csv") 
+
+#----------------------
+# 4-4 단골코드 정리하기
+#----------------------
+
+#---# 1) 날짜 만들기
+
+year <- 2011:2020
+month <- 2:11
+day <- 3: 12
+date <- data.frame(cbind(year, month, day))
+date <- date %>% mutate(ymd=make_date(year, month, day))
+head(date, 2)
+
+#---# 2) 여러단계를 거쳐서 다항식 계산하기
+
+a <- filter(mtcars, carb > 1)          # 계산 1: 필터링
+b <- group_by(a, cyl)                  # 계산 2: 그룹화
+c <- summarise(b, Avg_mpg = mean(mpg)) # 계산 3: 요약변수 생성
+d <- arrange(c, desc(Avg_mpg))         # 계산 4: 높은순서 정렬
+print(d)
+
+#---# 3) 파이프라인 연산자를 활용하여 다항식 연산을 한번에 계산하기
+
+mtcars %>%
+  filter(carb > 1) %>%                 # 계산 1: 필터링
+  group_by(cyl) %>%                    #         그룹화 
+  summarise(Avg_mpg = mean(mpg)) %>%   #         요약변수 생성
+  arrange(desc(Avg_mpg))               #         높은순서 정렬
+
+#---# 4) 특수문자 제거: 공백과 콤마 제거 
+
+library(stringr)
+tmp <- " 324,135"     # 공백과 쉼표가 포함된 문자열
+tmp %>% str_trim() %>% sub(",","",.) %>% as.numeric()  # 공백, 쉼표 제거
